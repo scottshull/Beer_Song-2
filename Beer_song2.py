f06c23a8a1feb3e9fb558d8c3b05561095b49f9a
scottshull <sshull@students.mchenry.edu>
2016-09-21 21:36:42 -0500

    Add files via upload

---
+++ Beer_song2.py
@@ -0,0 +1,7 @@
+number = 99
+
+while number >= 1:
+    print (str(number) + " bottles of beer on the wall, " + str(number) + " bottles of beer.")
+    print ("if one of those bottles should happen to fall")
+    number -= 1
+    print (str(number) + " bottles of beer on the wall.")
